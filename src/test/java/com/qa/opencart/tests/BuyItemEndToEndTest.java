462b7c6f9638c1e412be303ffbfb0ecda1621fce
Karol Kopyto <karol.kopyto@yahoo.com>
2023-06-30 22:17:10 +0200

    Class name changed

--- src/test/java/com/qa/opencart/tests/BuyItemEndToEndTest.java
+++ src/test/java/com/qa/opencart/tests/BuyItemEndToEndTest.java
@@ -2,6 +2,6 @@ package com.qa.opencart.tests;
 
 import com.qa.opencart.base.BaseTest;
 
-public class BuyItemEnsToEndTest extends BaseTest {
+public class BuyItemEndToEndTest extends BaseTest {
 
 }
